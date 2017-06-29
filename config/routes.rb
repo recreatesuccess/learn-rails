efb19609147f7274757bcea87e1be4fc8c51457d
Nonso Wogu <recreatesuccess@gmail.com>
2017-06-29 16:46:05 -0400

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
-Rails.application.routes.draw do
+#Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
+
+LearnRails::Application.routes.draw do
+
+  root to: 'visitors#new'
 end
