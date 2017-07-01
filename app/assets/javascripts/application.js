17037374d94ec4f655dd31416019f31d92110061
Nonso Wogu <recreatesuccess@gmail.com>
2017-07-01 11:35:51 -0400

    front-end framework

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+
+
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require foundation
 //= require_tree .
+
+$(function() {
+   $(document).foundation();
+ });
